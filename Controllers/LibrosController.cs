176738f5255958232b4ce87a1a5ada7758b87f7e
Dessu <brian.stiven.alvarado21@gmail.com>
2021-06-07 11:43:46 -0500

    CRUD simple - realizado con .net core

---
+++ Controllers/LibrosController.cs
@@ -0,0 +1,122 @@
+﻿using CrudNetCore5.Data;
+using CrudNetCore5.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudNetCore5.Controllers
+{
+    public class LibrosController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public LibrosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // Http Get Index
+        public IActionResult Index()
+        {
+            IEnumerable<Libro> listaLibros = _context.Libro;
+            return View(listaLibros);
+        }
+
+        // Http Get Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // Http Get Edit
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            // Obtener el libro
+            var libro = _context.Libro.Find(id);
+
+            if (libro == null)
+            {
+                return NotFound();
+            }
+
+            return View(libro);
+        }
+
+        // Http Get Delete
+        public IActionResult Delete(int? id)
+        {
+            // Obtener libro
+            var libro = _context.Libro.Find(id);
+
+            if (libro == null)
+            {
+                return NotFound();
+            }
+
+            return View(libro);
+        }
+
+        // Http Post Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Libro libro)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Libro.Add(libro);
+                _context.SaveChanges();
+
+                TempData["mensaje"] = "El libro se ha creado correctamente";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        // Http Post Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Libro libro)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Libro.Update(libro);
+                _context.SaveChanges();
+
+                TempData["mensaje"] = "El libro se ha actualizado correctamente";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        // Http Post Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteLibro(int? id)
+        {
+
+            //Obtener libro
+            var libro = _context.Libro.Find(id);
+
+            if(libro == null)
+            {
+                return NotFound();
+            }
+
+            _context.Libro.Remove(libro);
+            _context.SaveChanges();
+
+            TempData["mensaje"] = "El libro se ha eliminado correctamente";
+            return RedirectToAction("Index");
+
+        }
+
+    }
+}
